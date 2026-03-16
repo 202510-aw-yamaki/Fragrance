d8e8710bdc95af813233cd24b54af6b89c2fb661
LAPTOP-AIIROU83\academia <202510_aw_yamaki@x-ide.sakura.ne.jp>
2026-03-16 15:01:11 +0900

    企画案の中に仮のmavenファイルを追加

---
+++ 企画案md/仮fregrance/src/main/java/com/fregrance/FregranceApplication.java
@@ -0,0 +1,13 @@
+package com.fregrance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FregranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FregranceApplication.class, args);
+	}
+
+}
---
+++ 企画案md/仮fregrance/src/test/java/com/fregrance/FregranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fregrance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FregranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
