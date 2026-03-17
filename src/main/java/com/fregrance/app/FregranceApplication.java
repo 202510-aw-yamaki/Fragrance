44f626724ba681fe4299498a3b3ec8edc2bcbe24
LAPTOP-AIIROU83\academia <202510_aw_yamaki@x-ide.sakura.ne.jp>
2026-03-17 15:30:53 +0900

    バックエンド用に画像を追加、および参照記述変更

--- src/main/java/com/fregrance/app/FregranceApplication.java
+++ src/main/java/com/fregrance/app/FregranceApplication.java
@@ -1,9 +1,14 @@
 package com.fregrance.app;
 
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    MybatisAutoConfiguration.class
+})
 public class FregranceApplication {
 
     public static void main(String[] args) {
